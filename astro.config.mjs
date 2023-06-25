d6c4719c0cdb473cc7797d83d8614306fdf7e096
Jorge Pedroso <jpedroso@metaclassy.com>
2023-06-25 23:03:01 +0300

    Fix Astro config site param

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://metaclassy.com/',
-  base: '/metaclassy.com',
+  site: 'https://www.metaclassy.com/',
   integrations: [tailwind()]
 });
